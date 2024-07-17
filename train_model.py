3b631062e6ad14791eee506ce6648b8d4ca4dda5
Asa Gilmore <asaagilmore@gmail.com>
2024-07-17 14:15:23 -0700

    update train script

--- train_model.py
+++ train_model.py
@@ -5,18 +5,26 @@ import torch
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import argparse
-
+import logging
+import os
 
 if __name__ == "__main__":
     # setup parser
     parser = argparse.ArgumentParser(description='Training script')
     parser.add_argument('--data_dir', type=str, help='Path to the data '
                         'directory')
+    parser.add_argument('--run_name', type=str,
+                        help='Path to save model checkpoint ',
+                        default='training_run')
     args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     data_dir = args.data_dir
     train_dir = pjoin(data_dir, "train")
     val_dir = pjoin(data_dir, "valid")
+
+    checkpoint_path = args.run_name + ".pth"
 
     # def transforms
     train_transform = v2.Compose([
@@ -26,20 +34,27 @@ if __name__ == "__main__":
     ])
 
     train_dataset = nt.NiftiDataset(pjoin(train_dir, "images"),
-                                    pjoin(train_dir, "mask"),
+                                    pjoin(train_dir, "masks"),
                                     train_transform)
     val_dataset = nt.NiftiDataset(pjoin(val_dir, "images"),
                                   pjoin(val_dir, "masks"),
                                   train_transform)
 
     # setup dataloaders
-    train_loader = DataLoader(train_dataset, batch_size=50, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=50, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=20, shuffle=True,
+                              num_workers=10, prefetch_factor=4,
+                              pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=20, shuffle=False,
+                            num_workers=10, prefetch_factor=4,
+                            pin_memory=True)
 
+    logging.basicConfig(filename=args.run_name + '.log', level=logging.INFO,
+                        format='%(asctime)s:%(levelname)s:%(message)s')
     # setup combined perceptual loss with heavy bias toward MSE
-    loss = nt.losses.CombinedLoss(alpha=0.9, beta=0.1)
+    criterion = nt.losses.CombinedLoss(alpha=0.9, beta=0.1).to(device)
     # setup model
-    model = nt.models.UNet(in_channels=1, out_channels=1, ini_numb_filters=32)
+    model = nt.models.UNet(in_channels=1, out_channels=1,
+                           ini_numb_filters=32).to(device)
 
     def init_weights(m):
         if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
@@ -49,4 +64,62 @@ if __name__ == "__main__":
 
     model.apply(init_weights)
 
-    model.train_unet(train_loader, val_loader, num_epochs=300, criterion=loss)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+                                                optimizer,
+                                                mode='min',
+                                                patience=5)
+
+    if os.path.exists(checkpoint_path):
+        print("model checkpoint found, loading model")
+        checkpoint = torch.load(checkpoint_path)
+        model.load_state_dict(checkpoint["model_state_dict"])
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        scheduler.load_state_dict(checkpoint["scheduler_state_dict"])
+        start_epoch = checkpoint["epoch"]
+    else:
+        start_epoch = 0
+
+    for epoch in range(start_epoch, 300):
+        model.train()
+        train_loss = 0.0
+        total_samples = 0
+        for inputs, targets in train_loader:
+            inputs, targets = inputs.to(device), targets.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item() * inputs.size(0)
+            total_samples += inputs.size(0)
+
+        train_loss /= total_samples
+
+        model.eval()
+        val_loss = 0.0
+        total_samples = 0
+        for inputs, targets in val_loader:
+            inputs, targets = inputs.to(device), targets.to(device)
+
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            val_loss += loss.item() * inputs.size(0)
+            total_samples += inputs.size(0)
+
+        val_loss /= total_samples
+
+        scheduler.step(val_loss)
+
+        logging.info(f"Epoch: {epoch}, Train Loss: {train_loss}, "
+                     f"Val Loss: {val_loss},")
+
+        torch.save({
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "scheduler_state_dict": scheduler.state_dict(),
+            "epoch": epoch+1,
+        }, checkpoint_path)
